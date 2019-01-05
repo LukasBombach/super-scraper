22df389dc5fb6a694325b5648da8046dbd331fc0
Lukas Bombach <lukas.bombach@googlemail.com>
2019-01-05 14:04:25 +0100

    reboot with ts / rollup

---
+++ old/src/api.js
@@ -0,0 +1,19 @@
+function find(query) {
+  const {
+    name,
+    extension,
+    size,
+    hash,
+    title,
+    releaseDate,
+    platform,
+    description,
+    genres,
+    players,
+    coop,
+    rating,
+    images,
+    externalIds,
+    similarGames
+  } = query;
+}
